a9431815e4b2b41d132425f365d9c1f0b1d509a8
Prashant Kumar <144902971+Prashant2raja@users.noreply.github.com>
2026-04-24 10:30:40 +0530

    Update String_Palindrome.java

--- String_Palindrome.java
+++ String_Palindrome.java
@@ -7,20 +7,14 @@ class String_Palindrome {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String....");
         String s = sc.nextLine();
-
         int n = s.length();
-
         boolean ans = true;
-
         for (int i = 0; i < n / 2; i++) {
-
             if (s.charAt(i) != s.charAt(n - 1 - i)) {
                 ans = false;
                 break;
             }
-
         }
-
         if (ans) {
             System.out.println("Yesssss");
         } else {
